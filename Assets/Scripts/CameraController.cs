a6e2e2d8fad2b7622b65cae6e94606103baf3340
Adrian De Andrade <adriandeandrade98@gmail.com>
2018-10-23 21:09:49 -0400

    Implemented camera and basic player controls
    - Added camera controller
    -  Added player controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float cameraSpeed;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, cameraSpeed * Time.deltaTime);
+        transform.LookAt(target.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+
+    private Rigidbody rBody;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rBody.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        rBody.velocity = movement * moveSpeed * Time.fixedDeltaTime;
+    }
+
+}
